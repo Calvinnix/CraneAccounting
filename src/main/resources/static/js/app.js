2af481bcda7aa76f0943527b49ce0d8989009945
Calvin Nix <Calvin.Nix@pega.com>
2017-03-15 19:01:25 -0400

    Update

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2420,7 +2420,7 @@ var AllJournals = React.createClass({
                     <hr />
                     <select className="form-control" onChange={this.updateFilterJournalStatus} value={this.state.filterJournalStatus}>
                         <option value="">All</option>
-                        <option value="new">New</option>
+                        <option value="new">Pending</option>
                         <option value="approved">Approved</option>
                         <option value="rejected">Rejected</option>
                     </select>
